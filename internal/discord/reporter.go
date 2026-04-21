b7a6d0ef1b21cd67652451dd6b5e6bcff0e0c681
Alex <axel19911@gmail.com>
2026-04-21 05:32:29 +0300

    refactor

--- internal/discord/reporter.go
+++ internal/discord/reporter.go
@@ -91,7 +91,7 @@ func buildFields(report BossKillReport, ddBlocks []string, healBlocks []string) 
 	fields = append(fields, &discordgo.MessageEmbedField{
 		Name: "Общий DPS",
 		Value: fmt.Sprintf("```ansi\n%s%s%s%s\n```",
-			strings.Repeat(" ", 18),
+			strings.Repeat(" ", 20),
 			ansiColors["red_bold"],
 			FormatNum(report.TotalDps),
 			ansiColors["reset"]),
@@ -119,7 +119,7 @@ func buildFields(report BossKillReport, ddBlocks []string, healBlocks []string) 
 	fields = append(fields, &discordgo.MessageEmbedField{
 		Name: "Общий HPS",
 		Value: fmt.Sprintf("```ansi\n%s%s%s%s\n```",
-			strings.Repeat(" ", 18),
+			strings.Repeat(" ", 20),
 			ansiColors["green_bold"],
 			FormatNum(report.TotalHps),
 			ansiColors["reset"]),
