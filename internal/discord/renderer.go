957074d2e24e3b3719cbd8bb5e53706c760bb5c7
Alex <axel19911@gmail.com>
2026-04-21 03:16:12 +0300

    refactor

--- internal/discord/renderer.go
+++ internal/discord/renderer.go
@@ -105,7 +105,7 @@ func buildBlocks(players []PlayerReport, isDD bool, total int) []string {
 
 		// Формат: Ранг Ник (Дпс/Хпс) Рейтинг
 		// Используем \u2800 для пустого места (Braille Pattern Blank)
-		line := fmt.Sprintf("**%d**\u2800\u2800%s**%s** %s `[Топ: спек #%d]`\n",
+		line := fmt.Sprintf("**%d**\u2800\u2800%s%s `%s [Топ: спек #%d]`\n",
 			i+1, emoji, p.Name, FormatNum(val), p.SpecRank)
 
 		// Discord limit 1024 characters per field value. Safely cut at 1000.
