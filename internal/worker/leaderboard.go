6d408ebd25576936f1b8937bedd0908b3a5f1218
Alex <axel19911@gmail.com>
2026-04-28 12:32:18 +0300

    refactor

--- internal/worker/leaderboard.go
+++ internal/worker/leaderboard.go
@@ -47,7 +47,7 @@ func StartLeaderboardSync(store *storage.LeaderboardStorage) {
 
 							if len(players) == 0 {
 								fmt.Printf("[Worker] No player data for R:%d B:%d classID%d, specID%d, ilvlFrom%d, ilvlTo%d\n", raid.Order, bossID, classID, specID, ilvlFrom, ilvlTo)
-								time.Sleep(10 * time.Second)
+								time.Sleep(1 * time.Second)
 								continue
 							}
 
