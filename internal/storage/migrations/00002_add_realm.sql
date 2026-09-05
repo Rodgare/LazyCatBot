d3a436b6d0339993e88a6d5f126837ac4353428c
Alex <axel19911@gmail.com>
2026-09-05 03:50:35 +0300

    revert: remove bad 00003 migration, restore 00002

--- internal/storage/migrations/00002_add_realm.sql
+++ internal/storage/migrations/00002_add_realm.sql
@@ -1,13 +1,18 @@
 -- +goose Up
+-- +goose StatementBegin
 ALTER TABLE subscribe ADD COLUMN realm TEXT DEFAULT 'x3';
 ALTER TABLE player_subscribe ADD COLUMN realm TEXT DEFAULT 'x3';
 ALTER TABLE guild_members ADD COLUMN realm TEXT DEFAULT 'x3';
 ALTER TABLE leaderboard ADD COLUMN realm TEXT DEFAULT 'x3';
 ALTER TABLE actual_raids ADD COLUMN realm TEXT DEFAULT 'x3';
+-- +goose StatementEnd
 
 -- +goose Down
+-- +goose StatementBegin
 ALTER TABLE subscribe DROP COLUMN realm;
 ALTER TABLE player_subscribe DROP COLUMN realm;
 ALTER TABLE guild_members DROP COLUMN realm;
 ALTER TABLE leaderboard DROP COLUMN realm;
 ALTER TABLE actual_raids DROP COLUMN realm;
+-- +goose StatementEnd
+
