7257fc1c114ef7a6d89f4a32a49474422494b12b
Alex <axel19911@gmail.com>
2026-09-05 04:08:06 +0300

    fix: split 00002 ALTER TABLE into separate goose statement blocks

--- internal/storage/migrations/00002_add_realm.sql
+++ internal/storage/migrations/00002_add_realm.sql
@@ -1,18 +1,41 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE subscribe ADD COLUMN realm TEXT DEFAULT 'x3';
+-- +goose StatementEnd
+
+-- +goose StatementBegin
 ALTER TABLE player_subscribe ADD COLUMN realm TEXT DEFAULT 'x3';
+-- +goose StatementEnd
+
+-- +goose StatementBegin
 ALTER TABLE guild_members ADD COLUMN realm TEXT DEFAULT 'x3';
+-- +goose StatementEnd
+
+-- +goose StatementBegin
 ALTER TABLE leaderboard ADD COLUMN realm TEXT DEFAULT 'x3';
+-- +goose StatementEnd
+
+-- +goose StatementBegin
 ALTER TABLE actual_raids ADD COLUMN realm TEXT DEFAULT 'x3';
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 ALTER TABLE subscribe DROP COLUMN realm;
-ALTER TABLE player_subscribe DROP COLUMN realm;
-ALTER TABLE guild_members DROP COLUMN realm;
-ALTER TABLE leaderboard DROP COLUMN realm;
-ALTER TABLE actual_raids DROP COLUMN realm;
 -- +goose StatementEnd
 
+-- +goose StatementBegin
+ALTER TABLE player_subscribe DROP COLUMN realm;
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+ALTER TABLE guild_members DROP COLUMN realm;
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+ALTER TABLE leaderboard DROP COLUMN realm;
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+ALTER TABLE actual_raids DROP COLUMN realm;
+-- +goose StatementEnd
