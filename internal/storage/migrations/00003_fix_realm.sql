7bc7deef377737a22b7e33c85bb9c97b628d30bd
Alex <axel19911@gmail.com>
2026-09-05 03:39:39 +0300

    fix goose

--- internal/storage/migrations/00003_fix_realm.sql
+++ internal/storage/migrations/00003_fix_realm.sql
@@ -1,12 +1,10 @@
 -- +goose Up
-ALTER TABLE subscribe ADD COLUMN realm TEXT DEFAULT 'x3';
 ALTER TABLE player_subscribe ADD COLUMN realm TEXT DEFAULT 'x3';
 ALTER TABLE guild_members ADD COLUMN realm TEXT DEFAULT 'x3';
 ALTER TABLE leaderboard ADD COLUMN realm TEXT DEFAULT 'x3';
 ALTER TABLE actual_raids ADD COLUMN realm TEXT DEFAULT 'x3';
 
 -- +goose Down
-ALTER TABLE subscribe DROP COLUMN realm;
 ALTER TABLE player_subscribe DROP COLUMN realm;
 ALTER TABLE guild_members DROP COLUMN realm;
 ALTER TABLE leaderboard DROP COLUMN realm;
